c5b8eda279288304c9e9df88f50993a6d6f50a28
Brandon Jones <brandon.jones9@gmail.com>
2014-12-18 13:58:24 -0800

    damn

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Portfolio::Application.config.secret_key_base = '9b16a514bac33d0456e64320d0840306cf72c716188fe5a82047813760892208b95d16de07a5df7092d639f6de901a0b4da57829c9c4be631636fa79b393650a'
+Portfolio::Application.config.secret_key_base = ENV['PF_SECRETKEY']
