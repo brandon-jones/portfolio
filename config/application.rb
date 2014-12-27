138132614c5e167f56450bba474861a1a548627f
Brandon Jones <brandon.jones9@gmail.com>
2014-12-27 11:03:00 -0800

    more xframe stuff

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,6 @@ module Portfolio
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.action_dispatch.default_headers['X-Frame-Options'] = "ALLOW-FROM https://www.namecheap.com/"
+    config.action_dispatch.default_headers.merge!({'X-Frame-Options' => 'ALLOWALL'})
   end
 end
